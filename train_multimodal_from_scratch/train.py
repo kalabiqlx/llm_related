c3b6543867a2c9289a910689eace7534b27ce939
HUI <102224466+kalabiqlx@users.noreply.github.com>
2024-12-16 22:01:57 +0800

    Update train.py

--- train_multimodal_from_scratch/train.py
+++ train_multimodal_from_scratch/train.py
@@ -40,27 +40,28 @@ class VLM(PreTrainedModel):
         self.llm_model = AutoModelForCausalLM.from_pretrained(self.config.llm_model_path) # 自动加载自回归语言模型，无需手动指定模型类
         self.tokenizer = AutoTokenizer.from_pretrained(self.config.llm_model_path) # 加载语言模型的分词器
 
-        # 用于视觉与文本token的对齐，这里之所以有个*4是因为omni-vision中采用了对图像token的压缩，即将图像token序列的长度减小了4倍，同时增大了四倍的维度。
+        # 用于视觉与文本token的对齐，这里之所以有个*4是因为omni-vision中采用了对图像token的压缩（reshape），即将图像token序列的长度减小了4倍，同时增大了四倍的维度。
         self.linear1 = nn.Linear(self.vision_model.config.vision_config.hidden_size*4, self.llm_model.config.hidden_size)
         self.linear2 = nn.Linear(self.llm_model.config.hidden_size, self.llm_model.config.hidden_size)
+        
+        # 预训练只训练两个线性层(与LLaVA一致)
         if self.config.freeze_vision_model:
             for param in self.vision_model.parameters():
                 param.requires_grad = False
         for param in self.llm_model.parameters():
-            
             param.requires_grad = False
         
-    def forward(self, input_ids, labels, pixel_values, attention_mask=None):
-        text_embeds = self.llm_model.get_input_embeddings()(input_ids)
+    def forward(self, input_ids, labels, pixel_values, attention_mask=None): # 前向传播过程
+        text_embeds = self.llm_model.get_input_embeddings()(input_ids) # 返回模型的输入文本嵌入
         
-        image_embeds = self.vision_model.vision_model(pixel_values).last_hidden_state 
-        b, s, d = image_embeds.shape
+        image_embeds = self.vision_model.vision_model(pixel_values).last_hidden_state # 返回模型的输入图像嵌入
+        b, s, d = image_embeds.shape # (b, 14 * 14, d)
         image_embeds = image_embeds.view(b, -1, d*4)  # (b, 196, d) --> (b, 49, d*4) 压缩图片tokens
-        image_features = self.linear2(F.silu(self.linear1(image_embeds)))
+        image_features = self.linear2(F.silu(self.linear1(image_embeds))) # 图像token经过两层线性层之后得到的用于对齐的图像token序列
         
-        text_embeds = text_embeds.to(image_features.dtype)
+        text_embeds = text_embeds.to(image_features.dtype) # 将文本数据类型变为图像的数据类型
         
-        inputs_embeds = self.merge_input_ids_with_image_features(image_features, text_embeds, input_ids)
+        inputs_embeds = self.merge_input_ids_with_image_features(image_features, text_embeds, input_ids) 
         outputs = self.llm_model(inputs_embeds=inputs_embeds, attention_mask=attention_mask)
         logits = outputs[0]
         loss = None
@@ -73,14 +74,20 @@ class VLM(PreTrainedModel):
         
     def merge_input_ids_with_image_features(self, image_features, inputs_embeds, input_ids):
         
-        num_images, num_image_patches, embed_dim = image_features.shape
+        num_images, num_image_patches, embed_dim = image_features.shape # (b, 49, d)
         batch_indices, image_indices = torch.where(input_ids == self.tokenizer('<|image_pad|>')['input_ids'][0])
+        # 用分词器将文本中表示图像的特殊占位符<|image_pad|>分词
+        # 对整个 input_ids 张量进行比较，生成一个布尔矩阵，标记出等于 <|image_pad|> 的位置
+        # torch.where：找出布尔矩阵中为 True 的位置，返回两个索引数组：
+        # batch_indices：标识每个匹配位置所属的批次索引。
+        # image_indices：标识每个匹配位置在序列中的索引。
         
-        inputs_embeds[batch_indices, image_indices] = image_features.view(-1, embed_dim)
+        inputs_embeds[batch_indices, image_indices] = image_features.view(-1, embed_dim) # (b*49, d)
+        # 使用 batch_indices 和 image_indices 找到 inputs_embeds 中占位符<|image_pad|>对应位置，将这些位置的嵌入替换为image_features
         
         return inputs_embeds
     
-class MyDataset(Dataset):
+class MyDataset(Dataset): # 处理数据集
     def __init__(self, images_path, data_path, tokenizer, processor, config):
         super().__init__()
         self.data_path = data_path
@@ -89,33 +96,49 @@ class MyDataset(Dataset):
         self.processor = processor
         self.config = config
         with open(self.data_path, 'r', encoding='utf-8') as f:
-            self.datas = json.load(f)   
+            # 只读模式打开文件，指定文件编码为 UTF-8（确保可以正确读取包含中文、特殊字符等的文件）
+            # 使用 with 语句确保文件在操作完成后会自动关闭，避免资源泄露
+            self.datas = json.load(f)   # 将文件对象 f 中的 JSON 数据读取并解析为 Python 对象（通常是字典或列表）
         
             
     def __len__(self):
         return len(self.datas)
     
-    def __getitem__(self, index):
+    def __getitem__(self, index): 
+        # 这是 Python 数据加载类常用的方法（如继承自 torch.utils.data.Dataset）
+        # index：表示要访问的数据样本的索引
+        
         sample = self.datas[index]
         try:
             image_name = sample['image']
-            conversations = sample['conversations']
+            conversations = sample['conversations'] # 一组对话，包含用户和助手的内容。其中conversations[0]：用户输入（问题）。conversations[1]：助手回复（答案）
+
+            # 问题文本
+            # 使用 tokenizer 的 聊天模板 来格式化对话数据
             q_text = self.tokenizer.apply_chat_template([{"role":"system", "content":'You are a helpful assistant.'}, {"role":"user", "content":conversations[0]['value']}], \
-                tokenize=False, \
-                add_generation_prompt=True).replace('<image>', '<|image_pad|>'*self.config.image_pad_num)
+                tokenize=False, \ # 不立即进行分词，保持文本格式
+                add_generation_prompt=True).replace('<image>', '<|image_pad|>'*self.config.image_pad_num) 
+                # 添加一个生成文本的提示符，方便模型生成回复
+                # 将数据集中的图像占位符<image>替换为qwen2.5中图像占位符的表示<|image_pad|>
+
+            # 答案文本
+            # 将助手的回答（conversations[1]['value']）与结束符 eos_token 拼接在一起，标识回复的结束
             a_text = conversations[1]['value'] + self.tokenizer.eos_token
+            
             q_input_ids = self.tokenizer(q_text)['input_ids']
             a_input_ids = self.tokenizer(a_text)['input_ids']
             input_ids = q_input_ids + a_input_ids
-            labels = [tokenizer.pad_token_id] * len(q_input_ids) + a_input_ids
-            input_ids = input_ids[:-1]
-            labels = labels[1:]
+            labels = [tokenizer.pad_token_id] * len(q_input_ids) + a_input_ids # 问题部分的 token 用 pad_token_id 填充，因为这些位置不需要计算损失。回答部分使用真实的 a_input_ids 作为标签
+
+            # 为了实现 自回归语言模型 的训练方式，即：模型输入是前面的 token，目标是预测下一个 token
+            input_ids = input_ids[:-1] # 移除最后一个 token
+            labels = labels[1:] # 移除第一个 token
         
+            image = Image.open(os.path.join(self.images_path, image_name)).convert("RGB") # 使用 PIL（Python Imaging Library）打开指定路径下的图像文件并转换为RGB格式
+            pixel_values = self.processor(text=None, images=image)['pixel_values'] # 预处理图像数据
             
-            image = Image.open(os.path.join(self.images_path, image_name)).convert("RGB")
-            pixel_values = self.processor(text=None, images=image)['pixel_values']
-        except:
-            default_image = Image.new('RGB', (224, 224), color='white')
+        except: # 如果图像加载失败（例如文件不存在或读取错误），代码捕获异常
+            default_image = Image.new('RGB', (224, 224), color='white') # 创建一个默认的白色图像，尺寸为 224x224 像素
             pixel_values = self.processor(text=None, images=default_image)['pixel_values']
             q_text = self.tokenizer.apply_chat_template([{"role":"system", "content":'You are a helpful assistant.'}, {"role":"user", "content":"图片内容是什么\n<image>"}], \
                 tokenize=False, \
@@ -138,20 +161,26 @@ class MyDataset(Dataset):
 class MyDataCollator:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
-    
+
+    # __call__ 方法使得类实例可以像函数一样调用
+    # 输入是一个列表，每个元素是一个字典，代表一个样本。
+    # 每个样本包含以下键值对：
+    # input_ids：文本的 token ID 列表。
+    # labels：训练时的目标 token ID 列表。
+    # pixel_values：图像数据，通常是经过预处理的张量
     def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
-        max_len = max(len(feature['input_ids']) for feature in features)
+        max_len = max(len(feature['input_ids']) for feature in features) # 遍历所有样本获取input_ids 的最大长度
         input_ids = []
         labels = []
         pixel_values = []
         for feature in features:
-            input_ids.append(feature['input_ids'] + [self.tokenizer.pad_token_id] * (max_len - len(feature['input_ids'])))
+            input_ids.append(feature['input_ids'] + [self.tokenizer.pad_token_id] * (max_len - len(feature['input_ids']))) # 填充
             labels.append(feature['labels'] + [self.tokenizer.pad_token_id] * (max_len - len(feature['labels'])))
             pixel_values.append(feature['pixel_values'])
             
-        return {'input_ids': torch.tensor(input_ids, dtype=torch.long),
+        return {'input_ids': torch.tensor(input_ids, dtype=torch.long), # 将填充后的 input_ids 列表转换为 PyTorch 张量，并表示为长整型
                 'labels': torch.tensor(labels, dtype=torch.long),
-                'pixel_values': torch.cat(pixel_values, dim=0)}
+                'pixel_values': torch.cat(pixel_values, dim=0)} # 将批次中的所有图像张量拼接（concatenate）在一起, (batch_size, ...)
             
         
         
